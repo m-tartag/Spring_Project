7242264900cbfb5fd38be45b46959e26bd10f4dc
m-tartag <mattbrs51@gmail.com>
2020-02-21 11:29:18 -0500

    update

---
+++ src/main/java/com/scraper/Scraper.java
@@ -0,0 +1,6 @@
+package com.scraper;
+
+public class Scraper {
+
+
+}
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+
 /**
  * Unit test for simple App.
  */
@@ -28,30 +30,39 @@ public class AppTest {
         // Username Input
 
         WebElement userEntry = driver.findElement(By.id("login-username"));
-        userEntry.sendKeys("matt.tester24");
-        userEntry.submit();
+        userEntry.sendKeys("matt.tester24" + Keys.ENTER);
+//        userEntry.sendKeys(Keys.ENTER);
 
         // Password Input
 
         WebElement passEntry = new WebDriverWait(driver, 10)
                 .until(ExpectedConditions.elementToBeClickable(By.id("login-passwd")));
-        passEntry.sendKeys("112233aabbcc");
-        passEntry.sendKeys(Keys.ENTER);
+        passEntry.sendKeys("112233aabbcc" + Keys.ENTER);
+//        passEntry.sendKeys(Keys.ENTER);
 
         // Navigate to Finance -> My Portfolio
 
-            driver.get("https://finance.yahoo.com/portfolio/p_1/view");
+        driver.get("https://finance.yahoo.com/portfolio/p_1/view");
 
         // Target Table via "tbody" + Get Raw Text
 
         WebElement tableData = new WebDriverWait(driver, 10)
-                .until(ExpectedConditions.elementToBeClickable(By.tagName("tbody")));
+                .until(ExpectedConditions.elementToBeClickable(By.tagName("table")));
 
-        System.out.println(tableData.getText());
+//        System.out.println(tableData.getText());
 
+//        WebElement table = driver.findElement(By.tagName("table"));
+
+        String[] data;
+        List<WebElement> row = tableData.findElements(By.tagName("td"));
+        for (WebElement e : row) {
+            if (e.getText().length() != 0) {
+                System.out.println(e.getText());
+
+            }
+        }
 
     }
-
 }
 
 
